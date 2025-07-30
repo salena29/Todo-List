04052bc68178801e06971bf218f93a49098563c0
PunyaTamang <salenatamang257@gmail.com>
2025-07-30 11:12:02 +0545

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Todo from "./pages/Home";
+import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
@@ -11,9 +11,9 @@ const App = () => {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/todo" element={
+        <Route path="/home" element={
           <ProtectedRoute>
-            <Todo />
+            <Home />
           </ProtectedRoute>
         } />
       </Routes>
